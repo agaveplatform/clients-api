c4790e803e1d149a3c7f874317a52ca6039bd301
Joe Stubbs <jstubbs@tacc.utexas.edu>
2014-12-12 09:51:58 -0600

    Adding support for docker. All tests now pasing against the container.

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM ubuntu
+MAINTAINER Joe Stubbs
+
+RUN apt-get update
+RUN apt-get install -y python python-dev python-pip git
+RUN apt-get install -y apache2 apache2-mpm-prefork apache2-utils libexpat1 ssl-cert
+RUN apt-get install -y libapache2-mod-wsgi
+RUN apt-get install -y lynx
+RUN apt-get install -y libldap2-dev libsasl2-dev
+RUN apt-get install -y python-mysqldb
+
+RUN mkdir /code
+ADD requirements.txt /code/
+RUN pip install -r /code/requirements.txt
+
+# Manually set up the apache environment variables
+ENV APACHE_RUN_USER www-data
+ENV APACHE_RUN_GROUP www-data
+ENV APACHE_LOG_DIR /var/log/apache2
+ENV APACHE_LOCK_DIR /var/lock/apache2
+ENV APACHE_PID_FILE /var/run/apache2.pid
+
+ADD agave_clients /code/agave_clients/agave_clients
+ADD manage.py /code/agave_clients/
+RUN touch /code/agave_clients/agave_clients/running_in_docker
+RUN chmod o+rw -R /code/agave_clients/
+
+ADD deployment/wsgi.load /etc/apache2/mods-available/
+ADD deployment/apache2.conf /etc/apache2/sites-enabled/000-default.conf
+
+EXPOSE 80
+
+CMD /usr/sbin/apache2ctl -D FOREGROUND
--- agave_clients/local_settings_example.py
+++ agave_clients/local_settings_example.py
@@ -6,53 +6,19 @@
 # communicate with.
 TENANT_HOST = 'agave-staging.tacc.utexas.edu'
 
-# The ID of the tenant in the LDAP database. This needs to match the userstore configuration in APIM.
-# REMOVE?
-# APP_TENANT_ID = '1'
-
-# --------------------
-# JWT Header settings
-# -------------------
-# Whether or not to check the JWT; When this is False, certain features will not be available such as the
-# "me" lookup feature since these features rely on profile information in the JWT.
-# REMOVE?
-# CHECK_JWT = True
-
-# Actual header name that will show up in request.META; value depends on APIM configuration, in particular
-# the tenant id specified in api-manager.xml
-# REMOVE?
-# JWT_HEADER = 'HTTP_JWT_ASSERTION'
-
-# Relative location of the public key of the APIM instance; used for verifying the signature of the JWT.
-# REMOVE?
-# PUB_KEY = 'usersApp/agave-vdjserver-org_pub.txt'
-
-# APIM Role required to make updates to the LDAP database
-# REMOVE?
-# USER_ADMIN_ROLE = 'Internal/user-account-manager'
-
-# Whether or not the USER_ADMIN_ROLE before allowing updates to the LDAP db (/users service)
-# REMOVE?
-# CHECK_USER_ADMIN_ROLE = True
-
-# These settings are currently only used in the account sign up web application:
-# Deprecated - this is only used for manual testing of resolving the tenant id from the JWT.
-# REMOVE?
-# USE_APP_TENANT_ID = True
-# Tenantid key in WSO2 JWT Header:
-# WSO2_TID_STR = 'enduserTenantId":"'
-
-
 # ------------------------------
 # GENERAL SERVICE CONFIGURATION
 # ------------------------------
+# Base URL of this instance of the service. Used to populate the hyperlinks in the responses.
+APP_BASE = 'http://localhost:8000'
+
 # DEBUG = True turns up logging and causes Django to generate exception pages with stack traces and
 # additional information. Should be False in production.
 DEBUG = True
 
 # With this setting activated, Django will not create test databases for any database which has
 # the USE_LIVE_FOR_TESTS': True setting.
-TEST_RUNNER = 'testrunner.ByPassableDBDjangoTestSuiteRunner'
+TEST_RUNNER = 'agave_clients.testrunner.ByPassableDBDjangoTestSuiteRunner'
 
 # ----------------------
 # DATABASE CONNECTIVITY
