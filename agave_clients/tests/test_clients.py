8e08ded180378afe8984a55db0240a0283a5b830
Joe Stubbs <jstubbs@tacc.utexas.edu>
2016-03-22 12:17:44 -0500

    Porting test suite to py.test framework. All tests now passing.

--- agave_clients/tests/test_clients.py
+++ agave_clients/tests/test_clients.py
@@ -1,10 +1,23 @@
-# new test suite based on py.test.
+# Test suite based on py.test.
+# This test suite requires the following to have previously been setup:
+# 1. an (external) APIM manager installation, such as the dev.tenants.sandbox.agaveapi.co instance.
+# 2. A Test API registered and published in the APIM instance above with attributes given as in the sub_attrs fixture.
+# 3. clients service configuration pointing to the above APIM instance in the local_settings.py file (in particular,
+#    the tenant_host setting.)
+#
+#
+# To run the test suite:
 # Make sure the clients service is running on BASE_URL defined below. In particular, to run the tests against the
 # django development server, simply:
 # 1. activate a virtualenv with the requirements installed.
-# 2. start the dev server (e.g. python manage.py runserver 0.0.0.0:9000)
-# 3. point the test suite at the instance by updating BASE_URL=127.0.0.1:9000 or exporting base_url=127.0.0.1:9000
+# 2. Make sure test_settings.py is configured for the appropriate instance of APIM (see #3 above).
+# 3. start the dev server (e.g. python manage.py runserver 0.0.0.0:9000)
 # 4. run the tests by entering `py.test` in this directory.
+#
+# 5*.(optional) if the clients service is running in another location, point the test suite at it by exporting
+#     base_url=<location>:<port>
+# 6*. (optional). change the account used to authenticated to APIM by exporting username and password.
+
 
 import os
 import json
@@ -13,9 +26,22 @@ import base64
 import pytest
 import requests
 
+
 BASE_URL = os.environ.get('base_url', 'http://127.0.0.1:9000')
 username = os.environ.get('username', 'jdoe')
 password = os.environ.get('password', 'abcde')
+
+AGAVE_API_VERSION = 'v2'
+AGAVE_APIS = [{'name':'Apps', 'version':AGAVE_API_VERSION, 'provider':'admin'},
+            {'name':'Files', 'version':AGAVE_API_VERSION, 'provider':'admin'},
+            {'name':'Jobs', 'version':AGAVE_API_VERSION, 'provider':'admin'},
+            {'name':'Meta', 'version':AGAVE_API_VERSION, 'provider':'admin'},
+            {'name':'Monitors', 'version':AGAVE_API_VERSION, 'provider':'admin'},
+            {'name':'Notifications', 'version':AGAVE_API_VERSION, 'provider':'admin'},
+            {'name':'Postits', 'version':AGAVE_API_VERSION, 'provider':'admin'},
+            {'name':'Profiles', 'version':AGAVE_API_VERSION, 'provider':'admin'},
+            {'name':'Systems', 'version':AGAVE_API_VERSION, 'provider':'admin'},
+            {'name':'Transforms', 'version':AGAVE_API_VERSION, 'provider':'admin'},]
 
 
 # --------
@@ -33,6 +59,15 @@ def client_attrs():
                     'callbackUrl': 'http://localhost:9000/oauth/token',
                     'description': 'agave_clients testsuite client.'}
     return client_attrs
+
+@pytest.fixture(scope='session')
+def sub_attrs():
+    """Return attributes for the test subscription."""
+    sub_attrs = {'apiName': 'Test',
+                 'apiVersion': 'v0.1',
+                 'apiProvider': 'admin',
+                 'tier': 'Unlimited'}
+    return sub_attrs
 
 
 # ----------------
@@ -68,6 +103,15 @@ def validate_client(client, secret_present=False):
     assert 'callbackUrl' in client
     assert '_links' in client
 
+def validate_subscription(sub):
+    assert 'apiName' in sub
+    assert 'apiContext' in sub
+    assert 'apiProvider' in sub
+    assert 'apiStatus' in sub
+    assert 'apiVersion' in sub
+    assert 'tier' in sub
+    assert '_links' in sub
+
 def delete_client(client_attrs):
     """Ensure the test client is deleted in case of a previous failed run."""
     url = '{}/clients/v2/{}'.format(BASE_URL, client_attrs.get('clientName'))
@@ -102,6 +146,59 @@ def test_list_client_details(headers, client_attrs):
     assert client.get('name') == client_attrs.get('clientName')
     assert client.get('description') == client_attrs.get('description')
     assert client.get('callbackUrl') == client_attrs.get('callbackUrl')
+
+def test_list_subscriptions(headers, client_attrs):
+    url = '{}/clients/v2/{}/subscriptions'.format(BASE_URL, client_attrs.get('clientName'))
+    rsp = requests.get(url, headers=headers)
+    subs = validate_response(rsp)
+    assert len(subs) >= len(AGAVE_APIS)
+    for sub in subs:
+        validate_subscription(sub)
+        assert sub.get('apiName') in [api.get('name') for api in AGAVE_APIS]
+    for api in AGAVE_APIS:
+        assert api.get('name') in [sub.get('apiName') for sub in subs]
+
+def test_add_subscription(headers, client_attrs, sub_attrs):
+    url = '{}/clients/v2/{}/subscriptions'.format(BASE_URL, client_attrs.get('clientName'))
+    rsp = requests.post(url, data=sub_attrs, headers=headers)
+    validate_response(rsp)
+
+def test_ensure_added_subscription_present(headers, client_attrs, sub_attrs):
+    url = '{}/clients/v2/{}/subscriptions'.format(BASE_URL, client_attrs.get('clientName'))
+    rsp = requests.get(url, headers=headers)
+    subs = validate_response(rsp)
+    for sub in subs:
+        if sub.get('apiName') == sub_attrs.get('apiName'):
+            break
+    else:
+        # didn't find the subscription
+        assert False
+
+def test_delete_subscription(headers, client_attrs, sub_attrs):
+    url = '{}/clients/v2/{}/subscriptions'.format(BASE_URL, client_attrs.get('clientName'))
+    rsp = requests.delete(url, headers=headers, data=sub_attrs)
+    validate_response(rsp)
+
+def test_ensure_deleted_subscription_gone(headers, client_attrs, sub_attrs):
+    url = '{}/clients/v2/{}/subscriptions'.format(BASE_URL, client_attrs.get('clientName'))
+    rsp = requests.get(url, headers=headers)
+    subs = validate_response(rsp)
+    for sub in subs:
+        if sub.get('apiName') == sub_attrs.get('apiName'):
+            # found the test subscription
+            assert False
+
+def test_delete_all_subscriptions(headers, client_attrs):
+    url = '{}/clients/v2/{}/subscriptions'.format(BASE_URL, client_attrs.get('clientName'))
+    data = {'apiName': '*'}
+    rsp = requests.delete(url, headers=headers, data=data)
+    validate_response(rsp)
+
+def test_ensure_all_subscriptions_gone(headers, client_attrs):
+    url = '{}/clients/v2/{}/subscriptions'.format(BASE_URL, client_attrs.get('clientName'))
+    rsp = requests.get(url, headers=headers)
+    subs = validate_response(rsp)
+    assert len(subs) == 0
 
 def test_delete_client(headers, client_attrs):
     url = '{}/clients/v2/{}'.format(BASE_URL, client_attrs.get('clientName'))
